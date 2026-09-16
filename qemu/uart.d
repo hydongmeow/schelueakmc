51c502a22c508a7e71458c4727bb85ed77c42100
Huiyao/Iman <122889548+hydongmeow@users.noreply.github.com>
2026-09-16 09:59:01 +0300

    Delete qemu directory

--- qemu/uart.d
+++
@@ -1,2 +0,0 @@
-uart.o: uart.c uart.h
-uart.h:
--- qemu/uart.h
+++
@@ -1,8 +0,0 @@
-/* uart.h */
-#ifndef UART_H
-#define UART_H
-#include <stdint.h>
-void uart_putc(char c);
-void uart_puts(const char *s);
-void uart_putu32(uint32_t v);
-#endif
